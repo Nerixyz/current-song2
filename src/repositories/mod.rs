a06fb92fe7a44a07c2dd604e6a5f6b7bec90f2dc
Nerixyz <nero.9@hotmail.de>
2021-12-26 22:37:59 +0100

    fix: use compat middleware for cors

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -2,13 +2,14 @@ mod img;
 mod ws;
 
 use actix_cors::Cors;
-use actix_web::web;
+use actix_web::{middleware::Compat, web};
 
 pub fn init_repositories(config: &mut web::ServiceConfig) {
     config
         .service(web::scope("/img").configure(img::init_img))
         .service(
-            web::scope("/ws") /*.wrap(Cors::permissive())*/
+            web::scope("/ws")
+                .wrap(Compat::new(Cors::permissive()))
                 .configure(ws::init_ws),
         );
 }
