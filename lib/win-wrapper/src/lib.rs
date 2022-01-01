80e5c7bdbb552075e353cf0d5e48fbc5adb76976
Nerixyz <nero.9@hotmail.de>
2022-01-01 18:11:50 +0100

    fix: remove unused test

--- lib/win-wrapper/src/lib.rs
+++ lib/win-wrapper/src/lib.rs
@@ -3,18 +3,3 @@ pub mod elevate;
 pub mod message_box;
 pub mod path;
 mod pwstr;
-
-#[cfg(test)]
-mod tests {
-    use crate::message_box::{MessageBox, YesNoCancel};
-
-    #[test]
-    fn it_works() {
-        println!(
-            "{:?}",
-            MessageBox::<YesNoCancel>::error("forsen")
-                .with_title("aliens")
-                .show()
-        );
-    }
-}
