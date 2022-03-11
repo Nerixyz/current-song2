25693bb1726db5ca1e368cec905ab054db7eeb79
Nerixyz <nero.9@hotmail.de>
2022-03-11 23:39:58 +0100

    fix: remove console.logs

--- js/packages/extension/src/options-page/ToggleOption.ts
+++ js/packages/extension/src/options-page/ToggleOption.ts
@@ -1,4 +1,4 @@
-import { html, LitElement, PropertyDeclaration, PropertyValues } from 'lit';
+import { html, LitElement, PropertyValues } from 'lit';
 import { customElement, property, state } from 'lit/decorators';
 import { listenOption, Option, setOption } from '../options';
 
@@ -17,9 +17,7 @@ export class ToggleOption extends LitElement {
   protected updated(changedProperties: PropertyValues) {
     super.updated(changedProperties);
     if (!this._prevListener || changedProperties.has('option')) {
-      console.log('init');
       this._prevListener?.();
-      console.log(this.option);
       this._prevListener = listenOption<boolean>(this.option, value => {
         this._value = value;
         this.requestUpdate('_value');
@@ -28,7 +26,6 @@ export class ToggleOption extends LitElement {
   }
 
   protected render() {
-    console.log('render', this._value);
     return html`<label>
       ${this.label}
       <input type="checkbox" ?checked=${this._value} @change=${this._changed} />
@@ -36,7 +33,6 @@ export class ToggleOption extends LitElement {
   }
 
   private _changed(e: Event) {
-    console.log('changed');
     this._value = (e.target as HTMLInputElement).checked;
     setOption(this.option, this._value).catch(console.error);
   }
