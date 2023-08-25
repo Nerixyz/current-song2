e0799312a72ebf19e79bcfb4556b01d9009be349
Nerixyz <nerixdev@outlook.de>
2023-08-25 16:42:23 +0200

    fix: missing userscript in dev mode

--- js/client/src/user-scripts.ts
+++ js/client/src/user-scripts.ts
@@ -16,31 +16,35 @@ export function startUserScript(): UserScript {
   // Doing funky stuff with this import like importing it
   // at the start.
   const scriptPath = `/us${(0xe).toString(16)}r.js`;
-  import(scriptPath).then(fns => {
-    if (typeof fns !== 'object') return;
+  try {
+    import(scriptPath).then(fns => {
+      if (typeof fns !== 'object') return;
 
-    if (typeof fns.onPlay === 'function') {
-      script.onPlay = state => {
-        try {
-          fns.onPlay(state);
-        } catch (e) {
-          console.warn('Error in user script', e);
-        }
-      };
-    }
-    if (typeof fns.onPause === 'function') {
-      script.onPause = () => {
-        try {
-          fns.onPause();
-        } catch (e) {
-          console.warn('Error in user script', e);
-        }
-      };
-    }
-    if (typeof fns.default === 'function') {
-      fns.default();
-    }
-  });
+      if (typeof fns.onPlay === 'function') {
+        script.onPlay = state => {
+          try {
+            fns.onPlay(state);
+          } catch (e) {
+            console.warn('Error in user script', e);
+          }
+        };
+      }
+      if (typeof fns.onPause === 'function') {
+        script.onPause = () => {
+          try {
+            fns.onPause();
+          } catch (e) {
+            console.warn('Error in user script', e);
+          }
+        };
+      }
+      if (typeof fns.default === 'function') {
+        fns.default();
+      }
+    });
+  } catch (e) {
+    console.warn('Failed to load user-script', e);
+  }
 
   return script;
 }
